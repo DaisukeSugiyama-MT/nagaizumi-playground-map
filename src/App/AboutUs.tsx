b9329237e3debd5f57551108269fa9ff7cb3730d
DaisukeSugiyama-MT <sugiyama@modern-tech.co.jp>
2022-05-18 17:38:21 +0900

    update about us

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -1,40 +1,51 @@
-import './AboutUs.scss'
-import config from '../config.json'
-import { FaPlus } from 'react-icons/fa';
+import "./AboutUs.scss";
+import config from "../config.json";
+import { FaPlus } from "react-icons/fa";
 
 const Content = () => {
   const clickHandler = () => {
     if (config.form_url) {
-      window.location.href = config.form_url
+      window.location.href = config.form_url;
     }
-  }
-
+  };
 
   return (
     <div className="about-us">
       <div className="container">
         <div className="branding">
-          <div className="image"><img src={`${process.env.PUBLIC_URL}/icon-pwamap.svg`} alt=""/></div>
-          <div className="logo">Geolonia PWAマップ</div>
+          <div className="image">
+            <img src={`${process.env.PUBLIC_URL}/icon-pwamap.svg`} alt="" />
+          </div>
+          <div className="logo">長泉町公園地図アプリ</div>
         </div>
 
-        <p>Geolonia PWAマップは、Google スプレッドシートを更新するだけでオリジナルの地図アプリを作成することができます。</p>
-        <p>プログラムはオープンソースで公開しているため、自由にカスタマイズしてご利用いただけます。</p>
-        <p>また、独自ドメインでの利用やデザインのカスタマイズや利用に必要な緯度・経度情報の提供も有償で別途承ります。ご希望の方は下記までお問い合わせください。</p>
+        <p>長泉町公園地図アプリ</p>
+        <p>本アプリでは以下を利用しています。</p>
+        <p>
+          <a href=" https://geolonia.com/pressrelease/2022/05/17/pwamap.html">
+            Geolonia PWAマップ
+          </a>
+          <a href="https://opendata.pref.shizuoka.jp/dataset/8595.html">
+            ふじのくに オープンデータカタログ
+          </a>
+        </p>
 
-        <h2>Geolonia お問い合わせフォーム</h2>
-        <p><a href="https://geolonia.com/contact/">https://geolonia.com/contact/</a></p>
-        <p>※カスタマイズおよびアプリの作成・利用についてはサポート対象外となります。あらかじめご了承ください。</p>
-
-        {config.form_url?
+        {config.form_url ? (
           <>
             <h2>データの更新について</h2>
-            <p>このアプリのデータを更新するには下の「 + 」ボタンを押してフォームに必要な情報を入力してください。</p>
-            <div className="goto-form"><button><FaPlus color="#FFFFFF" onClick={clickHandler} /></button></div>
+            <p>
+              このアプリのデータを更新するには下の「 +
+              」ボタンを押してフォームに必要な情報を入力してください。
+            </p>
+            <div className="goto-form">
+              <button>
+                <FaPlus color="#FFFFFF" onClick={clickHandler} />
+              </button>
+            </div>
           </>
-          :
+        ) : (
           <></>
-        }
+        )}
       </div>
     </div>
   );
